d52bdaaa463a3216baee4f88f66eb054c99fefd6
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-08-01 09:22:31 -0600

    responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 
 function Card(props){
     return (
-        <div className="col-12 p-3 border my-3 col-md-3">
+        <div className="col-12 col-md-3 p-3 border my-3">
                 <i className={props.icon}/>
                 <h3>{props.cH}</h3>
                 <p>{props.para}</p>
